60f0fd272d8f3c5652989b2fd16181464db60235
andrei.mejov <andrei.mejov@bd780b84-f258-11de-99d0-77da0b7ae2c6>
2009-12-26 20:12:18 +0000

    Первая заливка

---
+++ trunk/Projects/MOMAdapters/MOMAdapters/ActiveMQAdapter.cs
@@ -0,0 +1,308 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: Mejov Andrei <andrei.mejov@gmail.com>
+ * Date: 18.08.2009
+ * Time: 21:35
+ * 
+ */
+using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Runtime.InteropServices;
+using Apache.NMS;
+using Apache.NMS.ActiveMQ;
+
+namespace MOMAdapters
+{
+	[ComVisible(true)]	
+	[ComSourceInterfacesAttribute(typeof(IAdapter))]	
+	[ClassInterface(ClassInterfaceType.AutoDual)]	
+	[ProgId("ActiveMQAdapter")]	
+	[Guid("0339E962-6744-4844-9408-1A795828CCB0")]	
+	public class ActiveMQAdapter : IAdapter
+	{
+		public static readonly string         ReceiveDestinationName = "ReceiveDestinationName";
+		public static readonly string            SendDestinationName = "SendDestinationName";
+		public static readonly string       ActiveMQConnectionString = "ActiveMQConnectionString";
+		public static readonly string                       UserName = "UserName";
+		public static readonly string                       Password = "Password";
+		public static readonly string 					      UseZip = "UseZip";
+		
+		private IConnection          connection = null;
+		private ISession                session = null;
+		private IDestination    sendDestination = null;
+		private IDestination receiveDestination = null;
+		private IMessageProducer       producer = null;
+		private IMessageConsumer       consumer = null;
+		private Queue<string>     messageBuffer = null;
+		private bool                 useZipFlag = false;
+		
+		private Dictionary<string, string>      parameters = new Dictionary<string, string>();
+	
+		[ComVisible(false)]
+		public Dictionary<string, string> Parameters
+		{
+			get {return parameters;}			
+		}
+		
+		public void SetParameter(string _name, string _value)
+		{
+			parameters.Add(_name, _value);
+		}
+		
+		public void ClearParameters()
+		{
+			parameters.Clear();
+		}		
+		
+		private IDestination CreateDestination(string _s)
+		{
+			string[] sa = _s.Split(new char[]{'/'});
+			if (sa.Length != 2)
+			{
+				throw new Exception("Illegal destination format: "+_s);
+			}
+			else
+			{
+				if (sa[0].Equals("queue"))
+				{
+					return session.GetQueue(sa[1]);
+				}
+				else
+				{
+					return session.GetTopic(sa[1]);
+				}
+			}
+		}
+		
+		public void Start()
+		{			
+			ValidateParameters();
+			ConnectionFactory factory = new ConnectionFactory(Parameters[ActiveMQConnectionString]);
+			if ((Parameters.ContainsKey("UserName")) && (Parameters.ContainsKey("Password")))
+			{
+				connection = factory.CreateConnection(Parameters["UserName"], Parameters["Password"]);
+			}
+			else
+			{
+				connection = factory.CreateConnection();
+			}
+			connection.Start();
+			session = connection.CreateSession(AcknowledgementMode.Transactional);
+						
+			if (Parameters.ContainsKey(SendDestinationName))
+			{
+				sendDestination = CreateDestination(Parameters[SendDestinationName]);
+				producer = session.CreateProducer(sendDestination);
+			}
+			
+			if (Parameters.ContainsKey(ReceiveDestinationName))
+			{
+				receiveDestination = CreateDestination(Parameters[ReceiveDestinationName]);
+				consumer = session.CreateConsumer(receiveDestination);						
+			}
+		
+			if (Parameters.ContainsKey(UseZip))
+			{
+				if (
+					(Parameters[UseZip].Equals("true")) || (Parameters[UseZip].Equals("True")) ||
+				    (Parameters[UseZip].Equals("1"))
+				   )
+				{
+					useZipFlag = true;				
+				}
+			}			
+			
+			messageBuffer = new Queue<string>();
+		}		
+		
+		private void ValidateParameters()
+		{
+			if (!((Parameters.ContainsKey(SendDestinationName)) || (Parameters.ContainsKey(ReceiveDestinationName))))
+			{
+				throw new Exception("SendDestinationName and/or ReceiveDestinationName must be set");
+			}
+			if ((Parameters.ContainsKey(UserName)) && (!Parameters.ContainsKey(Password)))
+			{
+				throw new Exception("Password must not be empty");
+			}
+		}
+
+		public void Stop()
+		{	
+			if (session != null)
+			{				
+				if (producer != null)
+				{
+					producer.Close();
+				}
+				producer = null;
+				if (consumer != null)
+				{
+					consumer.Close();
+				}
+				consumer = null;
+				session.Close();
+				session = null;
+				if (messageBuffer != null)
+				{
+					lock (messageBuffer)
+					{
+						messageBuffer.Clear();
+						messageBuffer = null;
+					}
+				}
+				connection.Stop();
+				connection.Close();
+				connection = null;
+				
+				useZipFlag = false;
+			}			
+		}
+		
+		public void SendFile(string _fileName)
+		{
+			using (FileStream fs = new FileStream(_fileName, FileMode.Open))
+			{
+				using (StreamReader sr = new StreamReader(fs, System.Text.UTF8Encoding.UTF8, true))
+				{
+					string buf = "";
+					while (!sr.EndOfStream) 
+					{
+						buf += sr.ReadLine();
+					}
+					Send(buf);
+				}
+			}
+		}
+
+		public void Send(string _text)
+		{			
+			if (producer == null)
+			{
+				throw new Exception("This connector not configured for sending");
+			}
+			IMessage message = null;
+			if (useZipFlag)
+			{
+				message = session.CreateBytesMessage(MSMQAdapter.CompressString(_text));
+			}
+			else
+			{
+				message = session.CreateTextMessage(_text);
+			}			
+			producer.Send(message);			
+		}
+		
+		private string ReceiveMessage(bool _noWait)
+		{		
+			IMessage message = null;
+			
+			if (_noWait)
+			{
+				message = consumer.ReceiveNoWait();
+				if (message == null)
+				{
+					return null;
+				}
+			}
+			else
+			{
+				message = consumer.Receive();
+			}
+			
+			if (useZipFlag)
+			{
+				return MSMQAdapter.DecompressString(((IBytesMessage)message).Content);
+			}
+			else if (message is ITextMessage)
+			{
+				return ((ITextMessage)message).Text;
+			}
+			else if (message is IBytesMessage)			
+			{				
+				byte[] buf = ((IBytesMessage)message).Content;
+				char[] chars = System.Text.UTF8Encoding.UTF8.GetChars(buf);
+				return new String(chars);
+			}
+			else
+			{
+				return message.ToString();
+			}
+		}
+		
+		public string Receive()
+		{		
+			if (consumer == null)
+			{
+				throw new Exception("This connector not configured for receiving");
+			}
+			lock (messageBuffer)
+			{
+				if (messageBuffer.Count > 0)
+				{
+					return messageBuffer.Dequeue();
+				}
+				else
+				{
+					return ReceiveMessage(false);
+				}
+			}
+		}
+		
+		public bool HasMessage()
+		{			
+			if (consumer == null)
+			{
+				throw new Exception("This connector not configured for receiving");
+			}
+			lock (messageBuffer)
+			{
+				if (messageBuffer.Count > 0)
+				{
+					return true;
+				}
+				else
+				{
+					string s = ReceiveMessage(true);
+					if (s != null)
+					{				
+						messageBuffer.Enqueue(s);
+						return true;
+					}
+					else
+					{
+						return false;
+					}
+				}
+			}
+		}
+		
+		public void Begin()
+		{			
+		}
+		
+		public void Commit()
+		{			
+			lock (messageBuffer)
+			{
+				messageBuffer.Clear();
+				session.Commit();
+			}
+		}
+		
+		public void Rollback()
+		{	
+			lock (messageBuffer)
+			{
+				messageBuffer.Clear();
+				session.Rollback();
+			}
+		}
+		
+		public void Dispose()
+		{
+			Stop();			
+		}
+	}
+}
---
+++ trunk/Projects/MOMAdapters/MOMAdapters/MSMQAdapter.cs
@@ -0,0 +1,258 @@
+/*
+ * Created by SharpDevelop.
+ * User: Mejov Andrei <andrei.mejov@gmail.com>
+ * Date: 01.01.2009
+ * Time: 15:34
+ *  
+ */
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.IO.Compression;
+using System.Messaging;
+using System.Text;
+using System.Threading;
+using System.Runtime.InteropServices;
+
+namespace MOMAdapters
+{		
+	[ComVisible(true)]	
+	[ComSourceInterfacesAttribute(typeof(IAdapter))]	
+	[ClassInterface(ClassInterfaceType.AutoDual)]	
+	[ProgId("MSMQAdapter")]	
+	[Guid("0339E962-6744-4844-9408-1A795828CCBE")]		
+	public class MSMQAdapter : IAdapter
+	{
+		public static readonly string ReceiveQueueName = "ReceiveQueueName";
+		public static readonly string    SendQueueName = "SendQueueName";
+		public static readonly string         UserName = "UserName";
+		public static readonly string         Password = "Password";
+		public static readonly string           UseZip = "UseZip";		
+		
+		private MessageQueue                  receiveQueue = null;
+		private MessageQueue                     sendQueue = null;
+		private MessageQueueTransaction        transaction = null;	
+		private bool                    transactionStarted = false;
+		private bool                            useZipFlag = false;	
+		
+		private Dictionary<string, string>      parameters = new Dictionary<string, string>();
+	
+		[ComVisible(false)]
+		public Dictionary<string, string> Parameters
+		{
+			get {return parameters;}			
+		}
+		
+		public void SetParameter(string _name, string _value)
+		{
+			parameters.Add(_name, _value);
+		}
+		
+		public void ClearParameters()
+		{
+			parameters.Clear();
+		}
+		
+		public void SendFile(string _fileName)
+		{
+			using (FileStream fs = new FileStream(_fileName, FileMode.Open))
+			{
+				using (StreamReader sr = new StreamReader(fs, System.Text.UTF8Encoding.UTF8, true))
+				{
+					string buf = "";
+					while (!sr.EndOfStream) 
+					{
+						buf += sr.ReadLine();
+					}
+					Send(buf);
+				}
+			}
+		}
+		
+		public void Send(string _text)
+		{
+			if (sendQueue == null)
+			{
+				throw new Exception("This connector not configured for sending");
+			}
+			if (!transactionStarted)
+			{
+				Begin();
+			}
+			if (useZipFlag)
+			{
+				sendQueue.Send(CompressString(_text), transaction);
+			}
+			else
+			{
+				sendQueue.Send(_text, transaction);
+			}
+		}
+		
+		public bool HasMessage()
+		{			
+			if (receiveQueue == null)
+			{
+				throw new Exception("This connector not configured for receiving");
+			}
+			IAsyncResult r = receiveQueue.BeginPeek();
+			Thread.Sleep(20);			
+			return r.IsCompleted;			
+		}
+		
+		public string Receive()
+		{
+			if (receiveQueue == null)
+			{
+				throw new Exception("This connector not configured for receiving");
+			}
+			if (!transactionStarted)
+			{
+				Begin();
+			}
+			
+			Message message = receiveQueue.Receive(transaction);			
+			if (useZipFlag)
+			{				
+				return DecompressString((byte[])(message.Body));
+			}				
+			else
+			{
+				return message.Body.ToString();
+			}			
+		}
+		
+		private void ValidateParameters()
+		{
+			if (!((Parameters.ContainsKey(SendQueueName)) || (Parameters.ContainsKey(ReceiveQueueName))))
+			{
+				throw new Exception("SendQueueName and/or ReceiveQueueName must be set");
+			}
+			if ((Parameters.ContainsKey(UserName)) && (!Parameters.ContainsKey(Password)))
+			{
+				throw new Exception("Password must not be empty");
+			}
+		}
+		
+		public void Start()
+		{
+			ValidateParameters();
+			transaction = new MessageQueueTransaction();
+			
+			if (MessageQueue.Exists(this.Parameters[ReceiveQueueName]))
+			{
+				receiveQueue = new MessageQueue(this.Parameters[ReceiveQueueName], true);
+			}
+			else
+			{
+				receiveQueue = MessageQueue.Create(this.Parameters[ReceiveQueueName], true);
+			}
+			if (MessageQueue.Exists(this.Parameters[SendQueueName]))
+			{
+				sendQueue = new MessageQueue(this.Parameters[SendQueueName], true);
+			}
+			else
+			{
+				sendQueue = MessageQueue.Create(this.Parameters[SendQueueName], true);
+			}
+			if (Parameters.ContainsKey(UseZip))
+			{
+				if (
+					(Parameters[UseZip].Equals("true")) || (Parameters[UseZip].Equals("True")) ||
+				    (Parameters[UseZip].Equals("1"))
+				   )
+				{
+					useZipFlag = true;
+					sendQueue.Formatter = new BinaryMessageFormatter();
+					receiveQueue.Formatter = new BinaryMessageFormatter();					
+				}
+			}			
+			if (!useZipFlag)
+			{
+				sendQueue.Formatter = new XmlMessageFormatter(new Type[]{typeof(string)});
+				receiveQueue.Formatter = new XmlMessageFormatter(new Type[]{typeof(string)});
+			}
+		}
+		
+		public void Stop()
+		{			
+			if (transactionStarted)
+			{
+				transaction.Abort();
+			}
+			receiveQueue.Dispose();			
+			sendQueue.Dispose();
+			
+			receiveQueue = null;
+			sendQueue = null;
+			
+			useZipFlag = false;
+		}
+		
+		public void Begin()
+		{
+			transaction.Begin();
+			transactionStarted = true;
+		}
+		
+		public void Commit()
+		{
+			if (transactionStarted)
+			{
+				transaction.Commit();
+				transactionStarted = false;
+			}
+		}
+		
+		public void Rollback()
+		{
+			if (transactionStarted)
+			{
+				transaction.Abort();
+				transactionStarted = false;
+			}
+		}
+
+		public static byte[] CompressString(string _s)
+		{
+			byte[] buf = UTF8Encoding.UTF8.GetBytes(_s);
+			using (MemoryStream ms = new MemoryStream())
+			{
+				using (DeflateStream ds = new DeflateStream(ms, CompressionMode.Compress))
+				{					
+					ds.Write(buf,0, buf.Length);
+					ds.Close();			
+					return ms.GetBuffer();
+				}
+			}
+		}
+						
+		public static string DecompressString(byte[] _bytes)
+		{			
+			using (MemoryStream ms = new MemoryStream(_bytes))
+			{				
+				using (DeflateStream ds = new DeflateStream(ms, CompressionMode.Decompress))
+				{					
+					using (MemoryStream resStream = new MemoryStream())
+					{						
+						int b = ds.ReadByte();
+						while (b>0)
+						{							
+							resStream.WriteByte((byte)b);
+							b = ds.ReadByte();
+						}						
+						byte[] buf = new byte[resStream.Position];						
+						resStream.Seek(0L, SeekOrigin.Begin);
+						resStream.Read(buf, 0, buf.Length);
+						return new string(UTF8Encoding.UTF8.GetChars(buf));
+					}//using resStream
+				}//using ds
+			}//using ms		
+		}
+		
+		public void Dispose()
+		{
+			Stop();
+		}
+	}
+}
