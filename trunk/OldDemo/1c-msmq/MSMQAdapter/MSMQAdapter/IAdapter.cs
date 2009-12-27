d1db38dab6981f5e48bac9627588e8fe805ef837
andrei.mejov <andrei.mejov@bd780b84-f258-11de-99d0-77da0b7ae2c6>
2009-12-27 13:23:40 +0000

    Создана папака OldDemo, в которую буду помещаться старые примеры и статьи к ним. В OldDemo добавлен пример 1c-msmq.

---
+++ trunk/OldDemo/1c-msmq/MSMQAdapter/MSMQAdapter/IAdapter.cs
@@ -0,0 +1,31 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: Mejov Andrei <andrei.mejov@gmail.com>
+ * Date: 01.01.2009
+ * Time: 23:25
+ *  
+ */
+using System;
+using System.Runtime.InteropServices;
+
+namespace MSMQAdapter
+{
+	[ComVisible(true)]
+	public interface IAdapter
+	{
+		void SetParameter(string _name, string _value);
+		void ClearParameters();
+		
+		void SendFile(string _text);
+		void Send(string _text);
+		bool HasMessage();
+		string Receive();
+		
+		void Start();
+		void Stop();
+		
+		void Begin();
+		void Commit();
+		void Rollback();
+	}
+}
---
+++ trunk/OldDemo/1c-msmq/MSMQAdapter/MSMQAdapter/TestAdapters.cs
@@ -0,0 +1,31 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: Mejov Andrei <andrei.mejov@gmail.com>
+ * Date: 04.07.2009
+ * Time: 13:57
+ *  
+ */
+
+using System;
+using NUnit.Framework;
+
+namespace MSMQAdapter
+{
+	[TestFixture]
+	public class TestAdapters
+	{
+		[Test]
+		public void TestJabber()
+		{
+			JabberAdapter center = new JabberAdapter();
+			center.SetParameter("ReceiveAddress", "center@192.168.1.11");			
+			center.SetParameter("SendAddress", "exchange@192.168.1.11");			
+			center.SetParameter("Server", "192.168.1.11:5222");
+			center.SetParameter("Password", "123456");
+			
+			center.Start();
+			center.Send("QQQ");
+			center.Stop();
+		}
+	}
+}
