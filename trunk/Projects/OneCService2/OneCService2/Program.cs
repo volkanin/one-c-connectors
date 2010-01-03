7d620b36c25cf41085e3313fbaa53cf3970ebcc9
andrei.mejov <andrei.mejov@bd780b84-f258-11de-99d0-77da0b7ae2c6>
2010-01-03 17:00:43 +0000

    (Update issue 1)
    Создан скелет OneCService2

---
+++ trunk/Projects/OneCService2/OneCService2/Program.cs
@@ -0,0 +1,27 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: Андрей
+ * Date: 03.01.2010
+ * Time: 19:58
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+using System;
+using System.Collections.Generic;
+using System.ServiceProcess;
+using System.Text;
+
+namespace OneCService2
+{
+	static class Program
+	{
+		/// <summary>
+		/// This method starts the service.
+		/// </summary>
+		static void Main()
+		{
+			// To run more than one service you have to add them here
+			ServiceBase.Run(new ServiceBase[] { new OneCService2() });
+		}
+	}
+}
