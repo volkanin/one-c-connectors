e89cbd69499d51da175cb36ed5ddf44358e2e871
andrei.mejov <andrei.mejov@bd780b84-f258-11de-99d0-77da0b7ae2c6>
2010-01-03 16:08:29 +0000

    (Update issue 2)
    Добавлены файлы для регистрации (отмены регистрации) MOMAdapters как COM, но почему-то пока не работает (Sharpdevelop видит их как COM, а 1С нет)

---
+++ Projects/dist.bat
@@ -0,0 +1,12 @@
+mkdir ..\Dist
+
+rem *************************** MOMAdapters *********************************
+
+mkdir ..\Dist\MOMAdapters
+
+copy .\MOMAdapters\MOMAdapters\reg\*.bat ..\Dist\MOMAdapters
+
+copy .\MOMAdapters\MOMAdapters\bin\Debug\* ..\Dist\MOMAdapters
+copy .\MOMAdapters\MOMAdapters\bin\Release\* ..\Dist\MOMAdapters
+
+
