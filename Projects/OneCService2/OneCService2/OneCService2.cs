1b753bcffc1083fa42dc1634c27517befa7ab0e5
andrei.mejov <andrei.mejov@bd780b84-f258-11de-99d0-77da0b7ae2c6>
2010-01-03 17:00:43 +0000

    (Update issue 1)
    Создан скелет OneCService2

---
+++ Projects/OneCService2/OneCService2/OneCService2.cs
@@ -0,0 +1,57 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: Andrei Mejov <andrei.mejov@gmail.com>
+ * Date: 03.01.2010
+ * Time: 19:58
+ *  
+ */
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.ServiceProcess;
+using System.Text;
+
+namespace OneCService2
+{
+	public class OneCService2 : ServiceBase
+	{
+		public const string MyServiceName = "OneCService2";
+		
+		public OneCService2()
+		{
+			InitializeComponent();
+		}
+		
+		private void InitializeComponent()
+		{
+			this.ServiceName = MyServiceName;
+		}
+		
+		/// <summary>
+		/// Clean up any resources being used.
+		/// </summary>
+		protected override void Dispose(bool disposing)
+		{
+			// TODO: Add cleanup code here (if required)
+			base.Dispose(disposing);
+		}
+		
+		/// <summary>
+		/// Start this service.
+		/// </summary>
+		protected override void OnStart(string[] args)
+		{
+			// TODO: Add start code here (if required) to start your service.
+		}
+		
+		/// <summary>
+		/// Stop this service.
+		/// </summary>
+		protected override void OnStop()
+		{
+			// TODO: Add tear-down code here (if required) to stop your service.
+		}
+	}
+}
---
+++ Projects/OneCService2/OneCService2/ProjectInstaller.cs
@@ -0,0 +1,34 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: Андрей
+ * Date: 03.01.2010
+ * Time: 19:58
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Configuration.Install;
+using System.ServiceProcess;
+
+namespace OneCService2
+{
+	[RunInstaller(true)]
+	public class ProjectInstaller : Installer
+	{
+		private ServiceProcessInstaller serviceProcessInstaller;
+		private ServiceInstaller serviceInstaller;
+		
+		public ProjectInstaller()
+		{
+			serviceProcessInstaller = new ServiceProcessInstaller();
+			serviceInstaller = new ServiceInstaller();
+			// Here you can set properties on serviceProcessInstaller or register event handlers
+			serviceProcessInstaller.Account = ServiceAccount.LocalService;
+			
+			serviceInstaller.ServiceName = OneCService2.MyServiceName;
+			this.Installers.AddRange(new Installer[] { serviceProcessInstaller, serviceInstaller });
+		}
+	}
+}
