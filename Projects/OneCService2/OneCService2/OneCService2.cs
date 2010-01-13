d52b742f970f2ee6597c85c61ca8c393d80d559c
andrei.mejov <andrei.mejov@bd780b84-f258-11de-99d0-77da0b7ae2c6>
2010-01-13 19:28:10 +0000

    (Update issue 1)
    Создан механизм конфигурирования сервиса, добавлены скрипты для регистрации/удаления и запуска/останова сервиса.
    
    Пока не смог победить относительные пути в register.bar.

--- Projects/OneCService2/OneCService2/OneCService2.cs
+++ Projects/OneCService2/OneCService2/OneCService2.cs
@@ -29,29 +29,29 @@ namespace OneCService2
 			this.ServiceName = MyServiceName;
 		}
 		
-		/// <summary>
-		/// Clean up any resources being used.
-		/// </summary>
 		protected override void Dispose(bool disposing)
-		{
-			// TODO: Add cleanup code here (if required)
+		{			
 			base.Dispose(disposing);
 		}
-		
-		/// <summary>
-		/// Start this service.
-		/// </summary>
+				
 		protected override void OnStart(string[] args)
-		{
-			// TODO: Add start code here (if required) to start your service.
+		{		
+			SimpleLogger.CreateDefaultLogger(@"D:\out.log");
+			try
+			{
+				SimpleLogger.DefaultLogger.Info(Config.ServiceConfig.Host);			
+				SimpleLogger.DefaultLogger.Info(Config.Connections.ContainsKey("TestConnection").ToString());
+			}
+			catch (Exception _e)
+			{
+				SimpleLogger.DefaultLogger.Severe(_e.ToString());
+			}
 		}
 		
-		/// <summary>
-		/// Stop this service.
-		/// </summary>
+		
 		protected override void OnStop()
 		{
-			// TODO: Add tear-down code here (if required) to stop your service.
+			SimpleLogger.DefaultLogger.Dispose();
 		}
 	}
 }
--- Projects/dist.bat
+++ Projects/dist.bat
@@ -10,3 +10,12 @@ copy .\MOMAdapters\MOMAdapters\bin\Debug\* ..\Dist\MOMAdapters
 copy .\MOMAdapters\MOMAdapters\bin\Release\* ..\Dist\MOMAdapters
 
 
+rem *************************** OneCService2 ********************************
+
+mkdir ..\Dist\OneCService2
+mkdir ..\Dist\OneCService2\scripts
+
+copy .\OneCService2\OneCService2\scripts\* ..\Dist\OneCService2\scripts
+
+copy .\OneCService2\OneCService2\bin\Debug\* ..\Dist\OneCService2
+copy .\OneCService2\OneCService2\bin\Release\* ..\Dist\OneCService2
