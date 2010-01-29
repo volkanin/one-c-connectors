71da18ec2af7b9675a8b5b4df2d56844f01065c9
andrei.mejov <andrei.mejov@bd780b84-f258-11de-99d0-77da0b7ae2c6>
2010-01-29 18:13:36 +0000

    (Update issue 1) Добавлен guid, уникальный для каждого соединения (экземпляра адаптера). Добавлен класс пула соединений.

---
+++ Projects/OneCService2/OneCService2/ConnectionPool.cs
@@ -0,0 +1,21 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: Andrei Mejov <andrei.mejov@gmail.com>
+ * Date: 29.01.2010
+ * Time: 21:09
+ *  
+ */
+using System;
+
+namespace OneCService2
+{
+	public class ConnectionPool
+	{
+		public ConnectionPool()
+		{
+			
+		}
+		
+		
+	}
+}
