6b2bdb22f3da99ca477aff931b5de310401175b5
andrei.mejov <andrei.mejov@bd780b84-f258-11de-99d0-77da0b7ae2c6>
2010-01-29 20:10:52 +0000

    (Update issue 1)
    Сделал первый рабочий вариант ConnectionPool'а. С конфигурацией пока не стыковал. Написал юнит-тест.

---
+++ Projects/OneCService2/OneCService2/TestPool.cs
@@ -0,0 +1,106 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: Andrei Mejov <andrei.mejov@gmail.com>
+ * Date: 29.01.2010
+ * Time: 22:10
+ *  
+ */
+
+using System;
+using NUnit.Framework;
+
+namespace OneCService2
+{
+	[TestFixture]
+	public class TestPool
+	{		
+		
+		[Test]
+		public void TestLifecycle()
+		{
+			ConnectionPool pool = new ConnectionPool();
+			
+			pool.Parameters.Add(ConnectionPool.AdapterTypeParam, "OneCService2.V81Adapter");
+			pool.Parameters.Add(ConnectionPool.NameParam, "TestName");
+			pool.Parameters.Add(ConnectionPool.PoolSizeParam, "3");
+			pool.Parameters.Add(ConnectionPool.PoolUserNameParam, "PoolUserName");
+			pool.Parameters.Add(ConnectionPool.PoolPasswordParam, "PoolPassword");
+			
+			pool.Parameters.Add(V81Adapter.ModeParam, "File");
+			pool.Parameters.Add(V81Adapter.FileParam, @"C:\Work\1C\Test");
+		    pool.Parameters.Add(V81Adapter.UserNameParam, "");
+			pool.Parameters.Add(V81Adapter.PasswordParam, "");
+
+			try
+			{
+				pool.Init();			
+			}
+			finally
+			{
+				pool.Done();
+			}
+			
+			Assert.IsTrue(true);
+		}
+		
+		[Test]
+		public void TestGetAndReleaseConnections()
+		{												
+			ConnectionPool pool = new ConnectionPool();
+			pool.Logger = new ConsoleLogger();
+			
+			pool.Parameters.Add(ConnectionPool.AdapterTypeParam, "OneCService2.V81Adapter");
+			pool.Parameters.Add(ConnectionPool.NameParam, "TestName");
+			pool.Parameters.Add(ConnectionPool.PoolSizeParam, "3");
+			pool.Parameters.Add(ConnectionPool.PoolUserNameParam, "PoolUserName");
+			pool.Parameters.Add(ConnectionPool.PoolPasswordParam, "PoolPassword");
+			
+			pool.Parameters.Add(V81Adapter.ModeParam, "File");
+			pool.Parameters.Add(V81Adapter.FileParam, @"C:\Work\1C\Test");
+			pool.Parameters.Add(V81Adapter.UserNameParam, "");
+			pool.Parameters.Add(V81Adapter.PasswordParam, "");
+
+			try
+			{
+				pool.Init();			
+				
+				/*Неправильная аутентификация*/
+				try
+				{
+					AbstractAdapter adapter = pool.GetConnection("AAA", "BBB");
+					Assert.IsTrue(false);
+				}
+				catch (Exception _e)
+				{
+					Console.WriteLine(_e.ToString());
+				}
+				
+				AbstractAdapter adapter1 = pool.GetConnection("PoolUserName", "PoolPassword");
+				AbstractAdapter adapter2 = pool.GetConnection("PoolUserName", "PoolPassword");
+				AbstractAdapter adapter3 = pool.GetConnection("PoolUserName", "PoolPassword");
+				
+				try
+				{
+					/*Должно отвалится по таймауту*/
+					pool.GetConnection("PoolUserName", "PoolPassword");
+					Assert.IsTrue(false);
+				}
+				catch (Exception _e)
+				{
+					Console.WriteLine(_e.ToString());
+				}
+				
+				pool.ReleaseConnection(adapter1);
+				pool.ReleaseConnection(adapter2);
+				pool.ReleaseConnection(adapter3);								
+			}
+			finally
+			{
+				pool.Done();
+			}
+			
+			Assert.IsTrue(true);
+		}
+	}		
+	
+}
