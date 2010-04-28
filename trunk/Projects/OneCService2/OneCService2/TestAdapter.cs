35ef77ffee3e56dfaed080b9df8b7805a937e3ff
andrei.mejov <andrei.mejov@bd780b84-f258-11de-99d0-77da0b7ae2c6>
2010-04-28 18:38:43 +0000

    (Update issue 6)
    Доделан ExecuteMethod. В вики будет помещена обновленная версия кода для 1С. На следующем этапе надо будет полностью переработать юнит-тесты и заняться вырезанием ненужных после реструктуризации частей, а также дописать обработку различных частных ситуаций (вроде скрипта без результат или вызова процедуры через ExecuteMethod). После чего погонять в бесконечном цикле с использованием пула соединений, ну и наконец можно будет вернуться к восстановлению работоспособности сервиса и играм с OpenESB.

--- trunk/Projects/OneCService2/OneCService2/TestAdapter.cs
+++ trunk/Projects/OneCService2/OneCService2/TestAdapter.cs
@@ -310,6 +310,9 @@ namespace OneCService2
 				r = adapter.ExecuteScript("м = Новый Массив(); м.Добавить(\"ЙЦУКЕН\"); результат = м;");
 				XmlNode arrayNode = r.Rows[0].Values[0];
 				
+				r = adapter.ExecuteScript("с = Новый Структура(); с.Вставить(\"Ключ1\", \"Значение1\"); с.Вставить(\"Ключ2\",Справочники.Номенклатура.НайтиПоКоду(1)); результат = с;");
+				XmlNode structNode = r.Rows[0].Values[0];
+				
 				r = adapter.ExecuteRequest("ВЫБРАТЬ 1, \"Й\", Истина, ДатаВремя(1, 1, 1)");
 				Assert.IsTrue(r.ColumnTypes[0].Equals("DOUBLE"));
 				Assert.IsTrue(r.ColumnTypes[1].Equals("STRING"));
@@ -322,6 +325,10 @@ namespace OneCService2
 				r = adapter.ExecuteMethod("OneCService2_ТестовыйМетод2", new XmlNode[] {r.Rows[0].Values[0], arrayNode});
 				Console.WriteLine(r.Rows[0].Values[0].Value);
 				Assert.IsTrue(r.Rows[0].Values[0].Value.Equals("1|Й|ЙЦУКЕН"));
+				
+				r = adapter.ExecuteMethod("OneCService2_ТестовыйМетод3", new XmlNode[] {r.Rows[0].Values[0], structNode});
+				Console.WriteLine(r.Rows[0].Values[0].Value);
+				Assert.IsTrue(r.Rows[0].Values[0].Value.Equals("1|Й|ЙЦУКЕН|Значение1|Товар 1"));
 			}
 			finally
 			{
